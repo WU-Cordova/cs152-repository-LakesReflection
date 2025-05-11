b3b5571e96538a5f0668535b758415732353c2da
Echo OLeary <pdoleary@willamette.edu>
2025-05-11 02:32:03 +0000

    deque finsihed at least according to local tests

--- projects/project2/program.py
+++ projects/project2/program.py
@@ -1,10 +1,17 @@
-
+from dataclasses import dataclass
+import enum
+from datastructures.bag import Bag
+import random
 
 def main():
-    
-    print("Hello, World!")
-
-
-
-if __name__ == '__main__':
-    main()
+    decklist = []
+    for i in range(14):
+        for q in range(4):
+            decklist.append((i,q))
+    print(decklist)
+    num_decks = random.randint(1,4)
+    for _ in range(num_decks):
+        decklist+=decklist
+    new_bag=Bag(*decklist)
+if __name__ =='__main__':
+        main()
