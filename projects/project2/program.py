4b5d29aa4dd267fcb456f346a04d5004dc086336
Echo OLeary <pdoleary@willamette.edu>
2025-05-11 02:34:15 +0000

    deque finsihed at least according to local tests

--- projects/project2/program.py
+++ projects/project2/program.py
@@ -2,16 +2,17 @@ from dataclasses import dataclass
 import enum
 from datastructures.bag import Bag
 import random
-
+import copy
 def main():
-    decklist = []
+    single_deck = []
     for i in range(14):
         for q in range(4):
-            decklist.append((i,q))
-    print(decklist)
+            single_deck.append((i,q))
+    print(single_deck)
     num_decks = random.randint(1,4)
+    multideck = copy.deepycopy(single_deck)
     for _ in range(num_decks):
-        decklist+=decklist
+        multideck += multideck
     new_bag=Bag(*decklist)
 if __name__ =='__main__':
         main()
